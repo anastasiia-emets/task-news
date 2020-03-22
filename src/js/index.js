a742ad478580266ef21fc7c377302432b6cefe1f
Anastasiia <yemets.anastasiia.oss@gmail.com>
2020-03-22 11:17:52 +0200

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,18 @@
-function viewComment() {
+var mybutton = document.getElementById("myBtn");
 
-    var x = document.querySelector(".comment");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-};
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-var modal = document.querySelector(".modal");
-var btn = document.querySelector(".article__button");
-var span = document.getElementsByClassName("close")[0];
-var sbm = document.querySelector(".form__button");
-btn.onclick = function () {
-    modal.style.display = "block";
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-span.onclick = function () {
-    modal.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-sbm.onclick = function () {
-    modal.style.display = "none";
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
